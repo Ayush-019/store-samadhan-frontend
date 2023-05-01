957b8218ea26225b20c3be0567eeffcc177af150
Ayush-019 <gargayush2002@gmail.com>
2023-05-01 19:55:53 +0530

    link change

--- src/Utils/Constants/APIConstants.js
+++ src/Utils/Constants/APIConstants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 //   "http://localhost:8000/api/v1";
-export const BASE_URL = "https://store-samadhan-backend.vercel.app/api/v1";
+export const BASE_URL = "https://easy-store-backend.vercel.app/api/v1";
 export const CLOUD_URL = "https://shttps://easy-store-storage.onrender.com/images";
   // export const CLOUD_URL =
   //   "https://storesamadhan-storage.centralindia.cloudapp.azure.com/images";
